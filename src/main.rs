739b502ad616acb43b55909a48cf9e5bba3bdd55
poly000 <1348292515@qq.com>
2021-07-23 19:16:10 +0800

    ? syntax

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::io::{self, BufWriter, Write};
 use std::{thread, time};
 use terminal_size::{terminal_size, Width};
 
-fn main() {
+fn main() -> Result<(), std::io::Error> {
     let size = terminal_size();
     let mut terminal_width: usize = 80;
     if let Some((Width(w), _)) = size {
@@ -13,20 +13,21 @@ fn main() {
     let duration = time::Duration::from_millis(50);
     let mut sw = BufWriter::new(io::stdout());
     
-    for progess in 0..progress_bar_length {
+    for progress in 0..progress_bar_length {
         sw.write_fmt(format_args!(
             "\r[{}>{}]",
             "=".repeat(progress),
             " ".repeat(progress_bar_length - progress - 1)
-        ));
-        sw.flush();
+        ))?;
+        sw.flush()?;
         thread::sleep(duration);
     }
     
     sw.write_fmt(format_args!(
         "\r[{}]",
         "=".repeat(progress_bar_length)
-    ));
-    sw.flush();
+    ))?;
+    sw.flush()?;
     println!("\nCongratulation, your system is faster now!");
+    Ok(())
 }
